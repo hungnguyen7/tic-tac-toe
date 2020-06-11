f6c4b7b559ef861632c6d258271ef93062b845c2
hungnguyen7 <hungnguyenst.99@gmail.com>
2020-06-11 12:02:41 +0700

    Updates

---
+++ precache-manifest.98bf671e22390d2c2c48f9b3424d4259.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e8799b6437a031c688f7ff08ef3b0ad",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "1e9fbbfce242e2f8bb68",
+    "url": "/tic-tac-toe/static/css/main.b79d8a19.chunk.css"
+  },
+  {
+    "revision": "e3660b286d92f0993ec6",
+    "url": "/tic-tac-toe/static/js/2.141c23dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.141c23dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e9fbbfce242e2f8bb68",
+    "url": "/tic-tac-toe/static/js/main.c5a14b96.chunk.js"
+  },
+  {
+    "revision": "b83c3f5dfddd51f16016",
+    "url": "/tic-tac-toe/static/js/runtime-main.e9088102.js"
+  }
+]);
